6482ee77c9294b2b7a5dae01554b03ed0dbda423
Nour <mohamednour25@gmail.com>
2024-09-17 12:45:19 +0300

    Initial Commit

---
+++ BidingExample/BidingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidingExampleApp.swift
+//  BidingExample
+//
+//  Created by administrator on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BidingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
